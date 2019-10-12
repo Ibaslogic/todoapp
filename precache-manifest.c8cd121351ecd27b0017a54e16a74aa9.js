674047c3aee1bf520cc5e244d83d3aae92e14087
ibaslogic <ibaskunle@gmail.com>
2019-10-12 08:25:58 +0100

    Updates

---
+++ precache-manifest.c8cd121351ecd27b0017a54e16a74aa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcf34b24b2e2b01965d9e69d7a0fbb8",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "97138c0100238508252c",
+    "url": "/todoapp/static/css/main.68a51777.chunk.css"
+  },
+  {
+    "revision": "45f2e7d1c032b406779e",
+    "url": "/todoapp/static/js/2.991b768e.chunk.js"
+  },
+  {
+    "revision": "97138c0100238508252c",
+    "url": "/todoapp/static/js/main.4a9cde50.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
