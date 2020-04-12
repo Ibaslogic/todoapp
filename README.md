42cb9ad9c144eb2615db531d26c6876d7e90ea27
ibaslogic <ibaskunle@gmail.com>
2020-04-12 15:56:37 +0100

    code, readme updated

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ Test this app:
 
 [ibaslogic.github.io/todoapp](https://ibaslogic.github.io/todoapp/)
 
-You can find all of the INITIAL code for the todos project here.
+You can find the code for the todos project here.
 
-> In the **latest edition of the book** (out soon), you will find the updated code here:
->
-> And for the **React Hooks version of the code**, which is part of the new edition, check here for the code:
+> The **latest edition of this book** is now available on Amazon. And it includes the **React Hooks**
 >
 > **If you bought the first edition of the book and you want the latest edition, buzz me on Twitter, [@ibaslogic](https://twitter.com/ibaslogic) or send a mail, ibas@ibaslogic.com to get it for FREE.**
 
@@ -38,4 +36,4 @@ Finally, start your development server by running:
 npm start
 ```
 
-You should see this app in your browser address bar at [http://localhost:3000](http://localhost:3000)
+You should see this app in your browser address bar at [http://localhost:3000](http://localhost:3000).
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,58 +2,63 @@ import React from "react";
 import Todos from "./Todos";
 import Header from "./layout/Header";
 import AddTodo from "./AddTodo";
-import uuid from "uuid";
+// import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoApp extends React.Component {
   state = {
     todos: [
       {
-        id: uuid.v4(),
+        // id: uuid.v4(),
+        id: uuidv4(),
         title: "Setup development environment",
-        completed: true
+        completed: true,
       },
       {
-        id: uuid.v4(),
+        // id: uuid.v4(),
+        id: uuidv4(),
         title: "Develop website and add content",
-        completed: false
+        completed: false,
       },
       {
-        id: uuid.v4(),
+        // id: uuid.v4(),
+        id: uuidv4(),
         title: "Deploy to live server",
-        completed: false
-      }
-    ]
+        completed: false,
+      },
+    ],
   };
 
-  handleChange = id => {
+  handleChange = (id) => {
     this.setState({
-      todos: this.state.todos.map(todo => {
+      todos: this.state.todos.map((todo) => {
         if (todo.id === id) {
           todo.completed = !todo.completed;
         }
         return todo;
-      })
+      }),
     });
   };
 
-  deleteTodo = id => {
+  deleteTodo = (id) => {
     this.setState({
       todos: [
-        ...this.state.todos.filter(todo => {
+        ...this.state.todos.filter((todo) => {
           return todo.id !== id;
-        })
-      ]
+        }),
+      ],
     });
   };
 
-  addTodo = title => {
+  addTodo = (title) => {
     const newTodo = {
-      id: uuid.v4(),
+      // id: uuid.v4(),
+      id: uuidv4(),
       title: title,
-      completed: false
+      completed: false,
     };
     this.setState({
-      todos: [...this.state.todos, newTodo]
+      todos: [...this.state.todos, newTodo],
     });
   };
 
